d585bafd515dfc1386739ad21f89cdd88cafee34
Quviry <kogekao@ya.ru>
2022-04-02 23:17:17 +0300

    Setting protocol added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import logging
 import teleinterface
-from importlib import reload
+import settings
     
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    teleinterface.run()
+    if settings.DEBUG:
+        logging.basicConfig(level=logging.DEBUG)
+        try:
+            teleinterface.run()
+        except Exception as e:
+            logging.getLogger(__name__).error(e)
+    else:
+        logging.basicConfig(level=logging.INFO)
+        teleinterface.run()
